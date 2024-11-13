f40bcba64a73459391295491012419cc42aa9914
CHEELEONG_KHONG <khongcl1307@gmail.com>
2024-11-13 22:28:56 +0800

    Revert "TEST 456"
    This reverts commit 1d3cab7f5d9a37e46242b70b32b3427061a32ff4.

--- OneDrive - Asia Pacific University/Documents/NetBeansProjects/APU Hostel Management/src/TestManagerMenu.java
+++
@@ -1,6 +0,0 @@
-public class TestManagerMenu {
-    public static void main(String[] args) {
-        User manager = new Manager("admin", "admin123");
-        new ManagerMenu(manager).setVisible(true);
-    }
-}
